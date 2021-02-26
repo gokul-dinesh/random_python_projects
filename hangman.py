a89fe911a901f7310b09b0677aa0deedff6f43bd
gokul-dinesh <73645340+gokul-dinesh@users.noreply.github.com>
2021-02-26 09:36:22 +0400

    simply tried them_
    not yet completed

---
+++ hangman.py
@@ -0,0 +1,14 @@
+from words import word_list
+
+def get_word():
+    word_competition = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("let's play hangman!")
+    print(display_hangman(tries))
+    print(word_competition);'
+    print("\n")
+    
+    while not guessed and tries > 0:
